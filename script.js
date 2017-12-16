3fffdee24bd5b2c1a44a7702d8afb115e24202cb
oldmanneill <30272866+oldmanneill@users.noreply.github.com>
2017-12-16 07:22:24 -0500

    working drop, except for some math issues

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     function blueCanvas () {
@@ -24,46 +23,51 @@ $(document).ready(function() {
             }
         }
     }
-    blueCanvas();
-    slots();
 
- /*   function slowdrop(yCoordAndTimer){
+   function slowdrop(yCoordAndTimer){
         setTimeout(function(){
             blueCanvas();
             slots();
-            ctx.beginPath();
-            ctx.arc(391,yCoordAndTimer,29,0,2*Math.PI);
-            ctx.fillStyle="red";
-            ctx.fill();
-            ctx.stroke();
-
-            },5*(yCoordAndTimer+36));
-
+            for (k=0; k<6;k++){
+                behindBlueDrop(k,yCoordAndTimer);
+            }
+        },135*(yCoordAndTimer));
     }
-    for (i=-36;i<391;i++){
+    function behindBlueDrop (checkAllSix,movingY){
+        if (Math.abs((36+checkAllSix*71)-movingY)<58){
+            partial(107,movingY,107,36+checkAllSix*71,29);
+            //   partial(107,85,107,107,29);
+            //  $('p').text(Math.abs((36+checkAllSix*71)-movingY));
+        }
+    }
+    function partial (x1,y1,x2,y2,r){
+
+        var yp = (y2+y1)/2;
+        var xp = x1+(Math.sqrt((r*r)-((y2-y1)/2)^2));
+        var direction;
+        var startingAngle1 = Math.atan((yp-y1)/(xp-x1));
+        $('#one').text('yp is: '+yp);
+        $('#two').text('y1 is: '+y1);
+        $('#three').text('xp is: '+xp);
+        $('#four').text('startingAngle1 is: '+startingAngle1);
+        var endingAngle1 = (Math.PI)-startingAngle1;
+        var endingAngle2 = 0 - startingAngle1;
+        var startingAngle2 = (Math.PI)-endingAngle2;
+        if (y1<y2){
+            direction = false;
+        }else{
+            direction = true;
+        }
+        ctx.beginPath();
+        ctx.arc(x1, y1, r, startingAngle1, endingAngle1,direction);
+        ctx.arc(x2,y2,r,startingAngle2, endingAngle2, direction);
+        ctx.fillStyle = "#0f0";
+        ctx.fill();
+        ctx.stroke();
+    }
+    for (i=0;i<391;i++){
         slowdrop(i);
     }
-    */
- function partial (x1,y1,x2,y2,r){
-     var yp = (y2+y1)/2;
-     var xp = x1+Math.sqrt((r*r)-((y2-y1)*(y2-y1)));
-     var direction;
-     var startingAngle1 = Math.atan((yp-y1)/(xp-x1));
-     var endingAngle1 = (Math.PI)-startingAngle1;
-     var endingAngle2 = 0 - startingAngle1;
-     var startingAngle2 = (Math.PI)-endingAngle2;
-     if (y1<y2){
-         direction = false;
-     }else{
-         direction = true;
-     }
-     ctx.beginPath();
-     ctx.arc(x1, y1, r, startingAngle1, endingAngle1,direction);
-     ctx.arc(x2,y2,r,startingAngle2, endingAngle2, direction);
-     ctx.fillStyle = "#0f0";
-     ctx.fill();
-     ctx.stroke();
- }
-partial(107,85,107,107,29);
 
+   // partial(107,79,107,107,29);
 })

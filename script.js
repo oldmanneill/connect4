4cf76253fc751e3be0d26b633d608ea6780b2461
oldmanneill <30272866+oldmanneill@users.noreply.github.com>
2017-12-19 18:24:37 -0500

    bug free drop of a single tolken

--- script.js
+++ script.js
@@ -17,9 +17,11 @@ $(document).ready(function() {
             for (var j = 0; j < 6; j++) {
                 ctx.beginPath();
                 ctx.arc(36 + i * 71, 107 + j * 71, 29, 0, 2 * Math.PI);
+                ctx.strokeStyle = "#fff";
                 ctx.fillStyle = "#fff";
                 ctx.fill();
                 ctx.stroke();
+                ctx.closePath();
             }
         }
     }
@@ -34,19 +36,44 @@ $(document).ready(function() {
         ctx.fill();
         ctx.stroke();
     }
+    function top(x,y,r){
+        var y2 = 71;
+        var startingAngle;
+        var endingAngle;
+        if ((Math.abs(y2-y))>=29){
+            startingAngle=0;
+            endingAngle= 2*Math.PI;
+        }
+        else {
+            var changeInX = Math.sqrt((r*r)-((y2-y)*(y2-y)));
+            startingAngle=Math.atan((y2-y)/(changeInX));
+            endingAngle = Math.PI-startingAngle;
+        }
+        ctx.beginPath();
+        ctx.arc(x, y, r, startingAngle, endingAngle,true);
+        ctx.strokeStyle ="#0f0";
+        ctx.fillStyle = "#0f0";
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
    function slowdrop(yCoordAndTimer){
         setTimeout(function(){
             blueCanvas();
             slots();
             clearTop();
-            for (k=0; k<6;k++){
+            if (yCoordAndTimer<100){
+                top(107,yCoordAndTimer,29);
+
+            }
+            for (k=0; k<7;k++){
                 behindBlueDrop(k,yCoordAndTimer);
             }
-        },103*(yCoordAndTimer));
+        },.5*(yCoordAndTimer));
     }
     function behindBlueDrop (checkAllSix,movingY){
         if (Math.abs((36+checkAllSix*71)-movingY)<59){
-            partial(107,71+movingY,107+checkAllSix*71,29);
+            partial(107,movingY,36+checkAllSix*71,29);
         }
     }
     function partial (x,y1,y2,r){
@@ -73,11 +100,13 @@ $(document).ready(function() {
         ctx.beginPath();
         ctx.arc(x, y1, r, startingAngle1, endingAngle1,direction);
         ctx.arc(x,y2,r,startingAngle2, endingAngle2, direction);
+        ctx.strokeStyle ="#0f0";
         ctx.fillStyle = "#0f0";
         ctx.fill();
         ctx.stroke();
+        ctx.closePath();
     }
-    for (i=0;i<391;i++){
+    for (i=0;i<462;i++){
         slowdrop(i);
 
     }

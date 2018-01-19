411c9b8c5bed9820e85f801d1dd7744bbb5f5f09
oldmanneill <30272866+oldmanneill@users.noreply.github.com>
2018-01-19 13:55:28 -0500

    replaced hard coded slot numbers with variables

--- script-non-working.js
+++ script-non-working.js
@@ -1,28 +1,43 @@
-$(document).ready(function () {
-    var board = [(0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0)];
-    var color = -1;
-    var engineArr = [];
+//$(document).ready(function () {
+//    var board = [(0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0)];
+  //  var color = -1;
+//    var engineArr = [];
+
+  //  function startNewGame() {
+        
+ //       slots();
+  //      clearTop();
+  //  }
+//startNewGame();
+  /*  function mainEngine(whosTurn, boardPositions) {
+        whosTurn *= -1;
+        chipBeforeFall(320, whosTurn);
+        if (moveChipSideToSide(boardPositions)[0] == 1) {
+            for (var i = 30; i < 462; i++) {
+                slowdrop(i, track, color);
+            }
+        }
+    }
+    */
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    blueCanvas(0,500,71,500);
-    slots(0,7,6);//creates the white slots, 7 across, 6 down
-    chipBeforeFall(320, 1);
-    moveChipSideToSide()
 
-    function blueCanvas(x1,x2,y1,y2) {
+    function blueCanvas() {
         ctx.beginPath();
-        ctx.moveTo(x1,y1);
-        ctx.lineTo(x1,y2);
-        ctx.lineTo(x2,y2);
-        ctx.lineTo(x2,y1);
-        ctx.lineTo(x1,y1);
+        ctx.moveTo(0, 71);
+        ctx.lineTo(0, 500);
+        ctx.lineTo(500, 500);
+        ctx.lineTo(500, 71);
+        ctx.lineTo(0, 71);
         ctx.fillStyle = "#00004d";
         ctx.fill();
     }
-
-    function slots(columnStart,columnStop,rowStop) {
-        for (var i = columnStart; i < columnStop; i++) {
-            for (var j = 0; j < rowStop; j++) {
+    blueCanvas();
+/*
+    function slots() {
+    
+        for (var i = 0; i < 7; i++) {
+            for (var j = 0; j < 6; j++) {
                 ctx.beginPath();
                 ctx.arc(36 + i * 71, 107 + j * 71, 29, 0, 2 * Math.PI);
                 ctx.strokeStyle = "#fff";
@@ -68,13 +83,10 @@ $(document).ready(function () {
         ctx.closePath();
     }
 
-    function slowdrop(yCoordAndTimer, x) {
+    function slowdrop(yCoordAndTimer, x, color) {
         setTimeout(function () {
-       //     blueCanvas();
-            var colStart = (x-36)/71;//
-            var colStop = colStart + 1;
-            var roStop = 6;
-            slots(colStart,colStop,roStop);
+            blueCanvas();
+            slots();
             clearTop();
             if (yCoordAndTimer < 100) {
                 top(x, yCoordAndTimer, 29);
@@ -99,7 +111,8 @@ $(document).ready(function () {
             return;
         }
         var startingAngle1 = Math.atan((yp - y1) / (xp - x));
-        $('#one').text('yp is: ' + yp);
+        $('#one').text('track is: ' + y1);
+
         $('#two').text('y1 is: ' + y1);
         $('#three').text('xp is: ' + xp);
         $('#four').text('startingAngle1 is: ' + startingAngle1);
@@ -121,10 +134,10 @@ $(document).ready(function () {
         ctx.closePath();
     }
 
-    function chipBeforeFall(x) {
+    function chipBeforeFall(x, color) {
         ctx.beginPath();
         ctx.arc(x, 30, 29, 0, 2 * Math.PI);
-        if (color-1) {
+        if (color) {
             ctx.strokeStyle = "#f00";
             ctx.fillStyle = "#f00";
         } else {
@@ -138,18 +151,18 @@ $(document).ready(function () {
 
     function moveChipSideToSide() {
         var flag = 0;
-        document.addEventListener("mousedown", function startMovingChip() {
+        document.addEventListener("mousedown", function () {
             var x = event.pageX;
             var y = event.pageY;
             if (x > 291 && x < 349 && y > 0 && y < 59 && flag == 0) { //chip starts in the middle. if it is clicked, movement is triggered
-                document.addEventListener("mousemove", function moveChipSideways() {
+                document.addEventListener("mousemove", function () {
                     var x = event.pageX;
                     if (x > 29 && x < 470 && flag == 0) {
                         clearTop();
                         chipBeforeFall(x, 1);
                     }
                 });
-                document.addEventListener("mouseup", function mouseUp() {
+                document.addEventListener("mouseup", function () {
                     var x = event.pageX;
                     flag = 1;
                     var track = Math.round((x - 36) / 71); //track = which column to drop the chip into
@@ -157,21 +170,21 @@ $(document).ready(function () {
                         track = 0;
                     }
                     track = 36 + (track * 71);
-                    color *= -1;
-                    for (var i = 30; i < 462; i++) {
-                        if (flag == 1) {
-                            slowdrop(i, track);
-                        }
-                    }
+                    //                    var color = 0;
+                    //                   for (var i=30;i<462;i++){
+                    //                        slowdrop(i,track,color);
+                    //                    }
                 });
             }
         });
     }
-
-    /*
+    // blueCanvas();
+    //    slots();
+    //  chipBeforeFall(320,1);
+    //    moveChipSideToSide()
+    
     for (i=30;i<462;i++){
         slowdrop(i);
     }
-    */
-
-})
+     */
+//})
